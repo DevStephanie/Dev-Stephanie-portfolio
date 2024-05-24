e4d9b23dc3ffe56e84bdb9a5c09ea8fbd10706e6
Stephanie A <stephabdullah@gmail.com>
2024-05-24 18:47:09 -0400

    completed portfolio template

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import './App.css';
-import About from './components/About';
-import ArtDump from './components/ArtDump';
-import Experience from './components/Experience';
-import Intro from './components/Intro';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import Projects from './components/Projects';
-import Technologies from './components/Technologies';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import ArtDump from "./components/ArtDump";
+import Experience from "./components/Experience";
+import Intro from "./components/Intro";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Projects from "./components/Projects";
+import Technologies from "./components/Technologies";
 import "./styles/Root.css";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <div id="content">
-        <Intro />
-        <About />
-        <Technologies />
-        <Experience />
-        <Projects />
-        <ArtDump />
-        <Footer />
+    // <Router>
+      <div className="App">
+        <NavBar />
+        <div id="content">
+          
+           
+            <Routes>
+              <Route path="/" element = {<>
+               <Intro />
+              <About />
+              <Technologies />
+              <Experience />
+              <Projects />
+              
+              </>}></Route>
+              <Route path="/art" element={<ArtDump />}></Route>
+            </Routes>
+
+          <Footer />
+        </div>
       </div>
-    </div>
+    // </Router>
   );
 }
 
